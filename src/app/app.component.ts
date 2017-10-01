1c51bded390741a4c909bd1a72525240e590fd1c
sabdiel <sabdiel.rivera@gmail.com>
2017-10-01 11:45:11 -0700

    Initial Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+  billTotal: number = 100;
+  tipPercentage: number = 20;
+  splitNumber: number = 1;
+
+  get total(): number {
+    return this.billTotal * (1 + (this.tipPercentage / 100));
+  }
+
+  get tip(): number {
+    return this.billTotal * (this.tipPercentage / 100);
+  }
+
+  get split(): number {
+    return this.total / this.splitNumber;
+  }
+
 }
